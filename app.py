77126d22aa5ace66a937f42ab23e1aa1ffdaf36d
Deepak Rana <111626727+DeepakRana07@users.noreply.github.com>
2024-04-23 13:07:12 +0530

    make sure before starting this file you have created the  virtual environment

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_mailman import Mail, EmailMessage
+
+mail=Mail()
+
+
+app= Flask(__name__)
+app.config["MAIL_SERVER"]="smtp.fastmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USERNAME"]= "api"
+app.config["MAIL_PASSWORD"] ="25b7a0f013676e7c42135e5299aea119"
+app.config["MAIL_USE_TLS"]=True
+app.config["MAIL_USE_SSL"]=False
+
+mail=Mail(app)
+
+@app.route('/send')
+def sendmail():
+
+    msg= EmailMessage(
+
+        subject="fgsfdgsf",
+        recepient=['ranashimshong@gmail.com'],
+        body='This is a test email',
+        mail_from='mailtrap@demomailtrap.com'
+       
+
+    )
+    EmailMessage.html="<b> heyt paul </b> , sending you this email from  my <a href='https://google.com'>Flask app</a>,link if it works"
+    mail.send(EmailMessage)
+
+
+    return "Message is send!"
+
+
+if __name__=="__main__":
+    app.run(debug=True,port=7000)
+    
+
+
+
